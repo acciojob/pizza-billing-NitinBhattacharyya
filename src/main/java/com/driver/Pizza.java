522dc0720977956af6a55faf44102e759a526060
NitinBhattacharyya <42495409+NitinBhattacharyya@users.noreply.github.com>
2023-08-13 08:42:05 +0530

    Project Submission

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -5,9 +5,9 @@ public class Pizza {
     private int price;
     private Boolean isVeg;
     private String bill;
-    private int topping;
-    private int cheese;
-    private int bag;
+    private boolean topping;
+    private boolean cheese;
+    private boolean bag;
 
     public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
@@ -21,9 +21,9 @@ public class Pizza {
             price=400;
             bill="Base Price Of The Pizza: 400\n";
         }
-        topping=0;
-        cheese=0;
-        bag=0;
+        topping=false;
+        cheese=false;
+        bag=false;
     }
 
     public int getPrice(){
@@ -32,39 +32,57 @@ public class Pizza {
 
     public void addExtraCheese(){
         // your code goes here
-        cheese+=80;
-        price+=80;
+        if(!cheese)
+        {
+            cheese=true;
+            price+=80;
+        }
     }
 
     public void addExtraToppings(){
         // your code goes here
-        if(isVeg)
+        if(!topping)
         {
-            topping+=70;
-            price+=70;
-        }
-        else {
-            topping+=120;
-            price+=120;
+            if(isVeg)
+            {
+                topping=true;
+                price+=70;
+            }
+            else {
+                topping=true;
+                price+=120;
+            }
         }
     }
 
     public void addTakeaway(){
         // your code goes here
-        bag+=20;
-        price+=20;
+        if(!bag)
+        {
+            bag=true;
+            price+=20;
+        }
     }
 
     public String getBill(){
         // your code goes here
-        bill=bill+"Extra Cheese Added: "+cheese+"\n"+"Extra Toppings Added: "+topping+"\n";
-        if(bag!=0)
+        if(cheese)
         {
-            bill=bill+"Paperbag Added: "+bag+"\n"+"Total Price: "+price;
+            bill=bill+"Extra Cheese Added: "+80+"\n";
         }
-        else {
-            bill=bill+"Total Price: "+price;
+        if(isVeg && topping)
+        {
+            bill=bill+"Extra Toppings Added: "+70+"\n";
         }
+        else if(!isVeg && topping)
+        {
+            bill=bill+"Extra Toppings Added: "+120+"\n";
+        }
+        if(bag)
+        {
+            bill=bill+"Paperbag Added: "+20+"\n";
+        }
+        bill=bill+"Total Price: "+price+"\n";
         return this.bill;
     }
 }
